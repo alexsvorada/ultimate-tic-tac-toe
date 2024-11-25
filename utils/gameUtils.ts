89ab4c1683f322bc16b31a7fbfc85abc2da5d375
alexsvorada <alex@svorada.eu>
2024-11-25 01:18:18 +0100

    feat: modify checkWinner, add getDefaultGameState + serialize functions

--- utils/gameUtils.ts
+++ utils/gameUtils.ts
@@ -1,4 +1,4 @@
-import type { Square, SubBoard } from '../types/ticTacToe'
+import type { GameState, Square, SubBoard } from '../types/ticTacToe'
 
 export const WINNING_COMBINATIONS = [
 	[0, 1, 2],
@@ -14,7 +14,13 @@ export const WINNING_COMBINATIONS = [
 export function checkWinner(squares: Square[]): boolean {
 	return WINNING_COMBINATIONS.some((combination) => {
 		const [a, b, c] = combination
-		return squares[a].owner && squares[a].owner === squares[b].owner && squares[a].owner === squares[c].owner
+		return (
+			squares[a].owner &&
+			squares[b].owner &&
+			squares[c].owner &&
+			squares[a].owner.symbol === squares[b].owner.symbol &&
+			squares[a].owner.symbol === squares[c].owner.symbol
+		)
 	})
 }
 
@@ -22,7 +28,7 @@ export function checkDraw(squares: Square[]): boolean {
 	return squares.every((square) => square.owner !== null)
 }
 
-export function createInitialBoard(): SubBoard[] {
+function createInitialBoard(): SubBoard[] {
 	return Array(9)
 		.fill(null)
 		.map((_, boardId) => ({
@@ -37,4 +43,20 @@ export function createInitialBoard(): SubBoard[] {
 			winner: null,
 			isDraw: false,
 		}))
+}
+
+export function getDefaultGameState(): GameState {
+	return {
+		currentPlayerIndex: 0,
+		winner: null,
+		isDraw: false,
+		activeBoard: null,
+		availableBoards: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+		previewNextBoard: [],
+		board: createInitialBoard(),
+	}
+}
+
+export function serialize(message: any): string {
+	return JSON.stringify(message)
 }
